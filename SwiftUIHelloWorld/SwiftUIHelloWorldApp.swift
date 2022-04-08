365792cf11ece31ad0bdf26d43f052be0f8c1d12
Naresh Kukkala <nareshkukkala28@gmail.com>
2022-04-08 12:43:30 +0530

    initial commit

---
+++ SwiftUIHelloWorld/SwiftUIHelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHelloWorldApp.swift
+//  SwiftUIHelloWorld
+//
+//  Created by naresh kukkala on 07/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
